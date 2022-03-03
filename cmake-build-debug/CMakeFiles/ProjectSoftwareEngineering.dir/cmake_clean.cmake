a72555ce68b4c02ff0aaeca43351b1c957637cad
SaidDeWolf <saiddewolf@gmail.com>
2022-03-03 14:39:47 +0100

    startup

---
+++ cmake-build-debug/CMakeFiles/ProjectSoftwareEngineering.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectSoftwareEngineering.dir/main.cpp.obj"
+  "ProjectSoftwareEngineering.exe"
+  "ProjectSoftwareEngineering.exe.manifest"
+  "ProjectSoftwareEngineering.pdb"
+  "libProjectSoftwareEngineering.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjectSoftwareEngineering.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ProjectSoftwareEngineering.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ProjectSoftwareEngineering.
+# This may be replaced when dependencies are built.
